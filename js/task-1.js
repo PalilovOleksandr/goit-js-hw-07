0a457d98545364e7c87f86b917f8e1194b0f3248
Alexander Palilov <prostosanekone@gmail.com>
2025-03-07 12:16:53 +0200

    finish work task 1 and 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,15 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const listEl = document.querySelector('#categories');
+const addClasslistEl = listEl.classList.add('list');
+const itemElements = listEl.querySelectorAll('.item');
+console.log(`Number of categories: ${itemElements.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const result = itemElements.forEach(item => {
+  const titleClass = item.querySelector('h2').classList.add('title-list');
+  const titleEl = item.querySelector('h2').textContent;
+  const categoryElements = item.querySelectorAll('li');
+  for (const item of categoryElements) {
+    const addClassCategoryEl = item.classList.add('item-list');
+  }
+  console.log(`Category: ${titleEl}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
